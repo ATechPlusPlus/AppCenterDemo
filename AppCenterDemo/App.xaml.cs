f608cd7dc39dd20b9e9b3bbcebe4764909798f78
Abdul Mateen Khan <abdulmateen50@gmail.com>
2021-06-07 21:43:52 +0530

    Add project files.

---
+++ AppCenterDemo/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+namespace AppCenterDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("android=9e0a7c0e-4629-4765-845f-cdc25a62f738;" +
+                     "uwp={Your UWP App secret here};" +
+                     "ios=7de8d37b-3727-4770-a7f7-f3ec332cebcf",
+                     typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
